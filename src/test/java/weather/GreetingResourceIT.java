3fea09c3e7bbd9298292e0997d8871d39412e229
m4rcosD <mp.dias09@outlook.com>
2023-12-21 13:25:33 +0000

    structure

---
+++ src/test/java/weather/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package weather;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
